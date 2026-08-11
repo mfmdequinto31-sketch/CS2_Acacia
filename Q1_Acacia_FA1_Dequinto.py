30ee1b145202fb314da986994a217fa9b9d01629
mfmdequinto31-sketch <mfmdequinto31@src.pshs.edu.ph>
2026-08-11 09:37:36 +0800

    Update Q1_Acacia_FA1_Dequinto.py

--- Q1_Acacia_FA1_Dequinto.py
+++ Q1_Acacia_FA1_Dequinto.py
@@ -1,8 +1,14 @@
+
 import math
 print("Calculate the distance between two points")
+
+# input x1 and x2
 x1 = int(input("Enter x1:"))
 x2 = int(input("Enter x2:"))
+
+# input y1 and y2
 y1 = int(input("Enter y1:"))
 y2 = int(input("Enter y2:"))
-d = round(math.sqrt((pow((x2 - x1),2)) + pow((y2 - y1),2)),2)
+
+distance = round(math.sqrt((pow((x2 - x1),2)) + pow((y2 - y1),2)),2)
 print("The distance between two points is,", d)
